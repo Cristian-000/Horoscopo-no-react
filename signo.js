a2951f9c5f30e6536ebb0b79314f14cbe90a1867
Cristian Fernández <129860008+Cristian-000@users.noreply.github.com>
2024-08-06 11:35:37 -0300

    Update signo.js ajustes

--- signo.js
+++ signo.js
@@ -23,6 +23,10 @@ function mostrarResultado(data) {
     resultadoDiv.innerHTML = `
         <div class="card signo-card">
             <div class="card-body">
+                <h5 class="card-title">${data.sign}</h5>
                 <p class="card-text">${data.horoscope}</p>
+                <p class="card-text"><small class="text-muted">Fecha: ${data.date}</small></p>
             </div>
-        </
+        </div>
+    `;
+}
